fa2b35f45c98d299139a992f015410cee6487751
Emir Şimşek <emir.simsek@enuygun.com>
2024-08-15 14:01:31 +0300

    new modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CatsModule } from './modules/cats/cats.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { CatsModule } from './modules/cats/cats.module';
+import { ReservationsModule } from './modules/reservations/reservations.module';
 import { UsersModule } from './modules/users/users.module';
 import { WebCarsModule } from './modules/web-cars/web-cars.module';
-import { ReservationsModule } from './modules/reservations/reservations.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://emirer2024:maldomal123@sacarya.z7p5wrm.mongodb.net/?retryWrites=true&w=majority&appName=sacarya'), 
+    MongooseModule.forRoot(''),
     CatsModule,
     AuthModule,
     UsersModule,
     WebCarsModule,
-    ReservationsModule
+    ReservationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
